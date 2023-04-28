544e5860be8d39fdada783ec324176549e0ea858
LBorgess <lucas_rancer@hotmail.com>
2023-04-27 21:49:31 -0300

    App Moviens main

---
+++ app/src/main/java/br/com/etecia/apptangedco/MovieActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.apptangedco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movie_layout);
+    }
+}
